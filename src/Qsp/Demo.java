222989e00e2e39bdcf69c2c1f5bfb20464ecf0b8
CBT <mahesh.srikrishna@gmail.com>
2018-02-03 16:39:07 +0530

    first commit

---
+++ src/Qsp/Demo.java
@@ -0,0 +1,29 @@
+package Qsp;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+   @Test
+    public void openandClose()throws MalformedURLException
+{
+	/*URL url=new URL("http://localhost:4444/wd/hub");
+	DesiredCapabilities d=new DesiredCapabilities();
+	d.setBrowserName("chrome");
+	WebDriver driver=new RemoteWebDriver(url,d);
+	driver.get("http://www.google.com");
+	driver.quit();*/
+	   
+	   System.setProperty("webdriver.chrome.driver", "./exes/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.quit();
+}
+}
